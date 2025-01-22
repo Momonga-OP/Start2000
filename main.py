1d15f6ae8291d373b246f11cc30372089a5b40dc
Momonga-OP <latifdofus@gmail.com>
2025-01-22 05:04:44 +0100

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ logger = logging.getLogger(__name__)
 
 intents = discord.Intents.default()
 intents.message_content = True
+intents.members = True
+
 
 bot = commands.Bot(command_prefix='!', intents=intents)
 
