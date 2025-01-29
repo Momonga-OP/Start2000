e1071f241c1a19a8ce4430ba3c343e1cf0287074
Momonga-OP <latifdofus@gmail.com>
2025-01-29 23:54:29 +0100

    Update config.py

--- cogs/config.py
+++ cogs/config.py
@@ -1,5 +1,21 @@
 from .database import get_all_guilds
 
+# Configuration
+GUILD_ID = 1300093554064097400  # Replace with your guild ID
+PING_DEF_CHANNEL_ID = 1307429490158342256  # Replace with your ping channel ID
+ALERTE_DEF_CHANNEL_ID = 1300093554399645715  # Replace with your alert channel ID
+
+# French alert messages
+ALERT_MESSAGES = [
+    "🚨 {role} go def zebi !",
+    "⚔️ {role}, il est temps de défendre !",
+    "🛡️ {role} Défendez votre guilde !",
+    "💥 {role} est attaquée ! Rejoignez la défense !",
+    "⚠️ {role}, mobilisez votre équipe pour défendre !",
+    "🏹 Appel urgent pour {role} - La défense a besoin de vous !",
+    "🔔 {role}, votre présence est cruciale pour la défense !",
+]
+
 def get_guild_emojis_roles():
     """Fetch guild data from the database."""
     guilds = get_all_guilds()
