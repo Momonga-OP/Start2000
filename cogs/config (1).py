eb11c8e054524ddbe4c4a827bf8b5414ae928eb1
Momonga-OP <latifdofus@gmail.com>
2025-01-30 20:20:56 +0100

    Add files via upload

---
+++ cogs/config (1).py
@@ -0,0 +1,30 @@
+# Configuration
+GUILD_ID = 1300093554064097400  # Replace with your guild ID
+PING_DEF_CHANNEL_ID = 1307429490158342256  # Replace with your ping channel ID
+ALERTE_DEF_CHANNEL_ID = 1300093554399645715  # Replace with your alert channel ID
+
+# Guild emojis with IDs and corresponding role IDs
+GUILD_EMOJIS_ROLES = {
+    "GTO": {"emoji": "<:GTO:1307418692992237668>", "role_id": 1300093554080612363},
+    "LMDF": {"emoji": "<:lmdf:1307418765142786179>", "role_id": 1300093554080612364},
+    "Notorious": {"emoji": "<:notorious:1307418766266728500>", "role_id": 1300093554064097406},
+    "Percophile": {"emoji": "<:percophile:1307418769764651228>", "role_id": 1300093554080612362},
+    "Tilisquad": {"emoji": "<:tilisquad:1307418771882905600>", "role_id": 1300093554080612367},
+    "Crescent": {"emoji": "<:Crescent:1328374098262495232>", "role_id": 1300093554064097404},
+    "Deviance": {"emoji": "<:Deviance:1328387647386947664>", "role_id": 1300093554064097408},
+    "Academie": {"emoji": "<:Academie:1333147586986774739>", "role_id": 1300093554080612365},
+}
+
+# French alert messages
+ALERT_MESSAGES = [
+    "🚨 {role} go def zebi !",
+    "⚔️ {role}, il est temps de défendre !",
+    "🛡️ {role} Défendez votre guilde !",
+    "💥 {role} est attaquée ! Rejoignez la défense !",
+    "⚠️ {role}, mobilisez votre équipe pour défendre !",
+    "🏹 Appel urgent pour {role} - La défense a besoin de vous !",
+    "🔔 {role}, votre présence est cruciale pour la défense !",
+]
+
+# Test button configuration
+TEST_BUTTON_ID = 486652069831376943
